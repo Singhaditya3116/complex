008a2e3c8ad6cc37104b155901fb21d6360250a0
singhaditya3116 <singhaditya3116@gmail.com>
2026-08-07 13:41:19 +0530

    k8s initial setup

---
+++ k8s repo/server/keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+  redisHost: process.env.REDIS_HOST,
+  redisPort: process.env.REDIS_PORT,
+  redisTls: process.env.REDIS_TLS === "true",
+  pgUser: process.env.PGUSER,
+  pgHost: process.env.PGHOST,
+  pgDatabase: process.env.PGDATABASE,
+  pgPassword: process.env.PGPASSWORD,
+  pgPort: process.env.PGPORT,
+  pgSsl: process.env.PG_SSL === "true"
+};
